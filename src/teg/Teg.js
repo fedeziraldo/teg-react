e94c144c483336ee51c798358ea0f699da26103b
Fede <federrico04@gmail.com>
2021-06-09 21:10:44 -0300

    dotenv react

--- src/teg/Teg.js
+++ src/teg/Teg.js
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "https://teg-node.herokuapp.com";
+const ENDPOINT = process.env.REACT_APP_BACK;
 const axios = require('axios');
 
 async function getUsuarios() {
   try {
-    const response = await axios.get('https://teg-node.herokuapp.com/usuarios');
+    const response = await axios.get(`${ENDPOINT}/usuarios`);
     console.log(response);
   } catch (error) {
     console.error(error);
