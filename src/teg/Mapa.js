4c95922c84d61df3ac995de75180a475c6689698
Fede <federrico04@gmail.com>
2021-12-01 18:02:12 -0300

    imgs de agfica resuelto cosas del konva

--- src/teg/Mapa.js
+++ src/teg/Mapa.js
@@ -1,28 +1,38 @@
-import { Stage, Layer, Image } from 'react-konva';
+import { Stage, Layer, Image} from 'react-konva';
+import  Konva from 'konva';
 import useImage from 'use-image';
 
 const drawHitFromCache = (img) => {
     if (img) {
+        img.off('click');
         img.on("click", () =>{
             alert("lau cochi te amo")
         })
         img.cache();
+        img.filters([Konva.Filters.RGB]);
+        img["red"](128)
         img.drawHitFromCache();
     }
 };
 
 function Mapa() {
 
-    const [image] = useImage('lion.png');
+    const [image] = useImage('Etiopia.png');
+
+    const styleMapa = {
+        backgroundImage: 'url("mapa.jpg")',
+        width: '1600px',
+        height: '1182px'
+    }
 
     return (
-        <Stage width={window.innerWidth} height={window.innerHeight} style={{backgroundColor: 'white'}}>
+        <Stage width={1600} height={1182} style={styleMapa}>
             <Layer>
                 <Image 
                     image={image} 
-                    width={200}
-                    height={200}
                     ref={ (node) => { drawHitFromCache(node); }}
+                    width={300}
+                    height={150}
                 />
             </Layer>
         </Stage>
