60a6a91880ec73221f3d330bb9dcc64290b40be6
Fede Ziraldo <federrico04@gmail.com>
2022-02-11 17:34:44 -0300

    optimize cache

--- src/teg/Sala.js
+++ src/teg/Sala.js
@@ -1,177 +1,247 @@
-import { useEffect, useRef, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
+import { Stage, Layer, Image, Text } from 'react-konva';
+import { useNavigate, useParams } from 'react-router-dom';
+import Konva from 'konva';
 import socketIOClient from "socket.io-client";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Container, Form, Button, Row, Col, ListGroup } from 'react-bootstrap';
+import useImage from 'use-image';
+import { Alert, Button, Form, ListGroup, Container } from 'react-bootstrap';
+import JugadaInvalida from './JugadaInvalida';
 const ENDPOINT = process.env.REACT_APP_BACK;
 
-function Sala() {
+const colores = [
+  { red: 255 },
+  { green: 255 },
+  { blue: 255 },
+  { red: 255, green: 255 },
+  { red: 255, blue: 255 },
+  { green: 255, blue: 255 },
+]
 
-  const navigate = useNavigate()
-  const socketRef = useRef()
-  const [texto, setTexto] = useState("");
-  const [user, setUser] = useState({})
-  const [usuarios, setUsuarios] = useState([])
-  const [salas, setSalas] = useState([]);
-  const [isLoading, setLoading] = useState(true)
-
-  const handleTexto = e => {
-    setTexto(e.target.value);
-  };
-
-  const enviarMensaje = e => {
-    e.preventDefault()
-    socketRef.current.emit("texto", texto)
-    setTexto("")
+const drawHitFromCache = (img, p) => {
+  if (img) {
+    img.cache();
+    img.filters([Konva.Filters.RGB]);
+    const color = colores[p.jugador.numero]
+    color.red && img["red"](color.red)
+    color.green && img["green"](color.green)
+    color.blue && img["blue"](color.blue)
+    img.drawHitFromCache();
   }
+};
 
-  const logOut = () => {
-    localStorage.removeItem("token");
-    navigate("/")
-  }
+function Mapa() {
+  const params = useParams();
 
-  const crearSala = () => {
-    socketRef.current.emit("crearSala")
-  }
+  const mapa = useImage('../mapa.jpg')[0]
 
-  const unirseSala = sala => {
-    socketRef.current.emit("unirseSala", sala)
-  }
+  const imageRef = useRef()
 
-  const salirSala = () => {
-    socketRef.current.emit("salirSala")
-  }
+  // for (let i=0; i<72; i++) {
+  //   imageRefs.push(useRef())
+  // }
 
-  const eliminarSala = () => {
-    socketRef.current.emit("eliminarSala")
-  }
-
-  const crearJuego = () => {
-    socketRef.current.emit("crearJuego")
-  }
-
-  const irJuego = nombrejuego => {
-    navigate(`/mapa/${nombrejuego}`)
-  }
+  const images = {}
+  images['ANGOLA.png'] = useImage('../ANGOLA.png')[0]
+  images['EGIPTO.png'] = useImage('../EGIPTO.png')[0]
+  images['ETIOPIA.png'] = useImage('../ETIOPIA.png')[0]
+  images['MADAGASCAR.png'] = useImage('../MADAGASCAR.png')[0]
+  images['MAURITANIA.png'] = useImage('../MAURITANIA.png')[0]
+  images['NIGERIA.png'] = useImage('../NIGERIA.png')[0]
+  images['SAHARA.png'] = useImage('../SAHARA.png')[0]
+  images['SUDAFRICA.png'] = useImage('../SUDAFRICA.png')[0]
+  images['CUBA.png'] = useImage('../CUBA.png')[0]
+  images['EL_SALVADOR.png'] = useImage('../EL_SALVADOR.png')[0]
 
   useEffect(() => {
-    const getUsuarios = async () => {
-      console.log(await axios.get(`${ENDPOINT}/usuarios`))
+    if (images['ANGOLA.png']) {
+      imageRef.current.cache();
+      imageRef.current.getLayer().batchDraw();
     }
+  }, [images]);
 
+  const socketRef = useRef()
+
+  const [jugador, setJugador] = useState({})
+
+  const [juego, setJuego] = useState({
+    jugadores: [],
+    paises: [],
+    turno: {}
+  })
+
+  const [ataque, setAtaque] = useState({})
+
+  const [show, setShow] = useState(false)
+  const [jugadaInvalida, setJugadaInvalida] = useState("")
+  const navigate = useNavigate()
+
+  const handleAtaque = e => {
+    const a = { ...ataque }
+    a[e.target.name] = e.target.value
+    setAtaque(a);
+  };
+
+  const accionTerminarTurno = () => {
+    socketRef.current.emit('accionTerminarTurno')
+  };
+
+  const atacar = e => {
+    e.preventDefault()
+    socketRef.current.emit('accionDoble', ataque.numeroPaisO, ataque.numeroPaisD)
+  };
+
+  const botonEmpate = nombreJuego => {
+    socketRef.current.emit('botonEmpate', nombreJuego)
+  };
+
+  const volverASala = e => {
+    navigate("/sala")
+  };
+
+  useEffect(() => {
     const initSocket = () => {
-      socketRef.current = socketIOClient(`${ENDPOINT}/sala`)
+      socketRef.current = socketIOClient(`${ENDPOINT}/mapa`)
 
-      socketRef.current.emit('validacion', localStorage.getItem("token"))
+      socketRef.current.emit('validacion', localStorage.getItem("token"), params.nombreJuego)
 
       socketRef.current.on('loginIncorrecto', () => {
-        localStorage.removeItem("token");
-        navigate("/")
+        navigate("/sala")
       })
 
-      socketRef.current.on('loginCorrecto', user => {
-        setUser(user)
-        setLoading(false)
+      socketRef.current.on('loginCorrecto', jugador => {
+        setJugador(jugador)
       })
 
-      socketRef.current.on('salas', salas => {
-        setSalas(salas)
+      socketRef.current.on('juego', juego => {
+        setJuego(juego)
       })
 
-      socketRef.current.on('usuarios', usuarios => {
-        setUsuarios(usuarios)
-      })
-
-      socketRef.current.on('user', user => {
-        setUser(user)
-      })
-
-      socketRef.current.on("texto", texto => {
-        document.getElementById("chat").innerHTML += `<li>${texto}</li>`
-      })
-
-      socketRef.current.on("iniciarJuego", () => {
-        navigate("/mapa")
+      socketRef.current.on('jugadaInvalida', mensaje => {
+        setShow(true)
+        setJugadaInvalida(mensaje)
       })
     }
 
-    getUsuarios()
     initSocket()
 
     return () => {
       socketRef.current.disconnect();
     };
-  }, [navigate]);
+  }, [navigate, params]);
 
   const estilos = {
-    backgroundColor: 'white'
+    backgroundColor: 'white',
+    margin: '0px'
   }
 
   return (
     <Container style={estilos}>
-      <Row>
-        <Col>
-          <h2>Hola {user.usuario && user.usuario.email}</h2>
-          <Button variant="danger" onClick={logOut} disabled={isLoading}>Salir</Button>
-        </Col>
-        <Col>
-          <h2>Salas</h2>
-          <h3>Estas unido a la sala {user.nombreSala}</h3>
-          <ListGroup variant="flush">
-            {
-              Object.keys(salas).map(s =>
-                <ListGroup.Item key={salas[s].creador.usuario.email}>{salas[s].creador.usuario.email} {salas[s].participantes.length}
-                  <Button variant="primary" onClick={() => unirseSala(salas[s].creador.usuario.email)} disabled={isLoading}>Unirse a sala</Button>
-                </ListGroup.Item>
-              )}
-          </ListGroup>
-          <Button variant="success" onClick={crearSala} disabled={isLoading}>Crear sala</Button>
-            {
-              Object.keys(salas).map(s => salas[s].creador.usuario.email).includes(user.usuario && user.usuario.email) ?
-                <>
-                  <Button variant="danger" onClick={eliminarSala} disabled={isLoading}>Eliminar sala</Button>
-                  <Button variant="warning" onClick={crearJuego} disabled={isLoading}>Iniciar juego</Button>
-                </> :
-                <Button variant="danger" onClick={salirSala} disabled={isLoading}>Salir sala</Button>
-            }
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h2>Usuarios conectados</h2>
-          <ListGroup variant="flush">
-            {
-              usuarios.map(u => <ListGroup.Item key={u}>{u}</ListGroup.Item>)
-            }
-          </ListGroup>
-        </Col>
-        <Col>
-          <ul id="chat"></ul>
-          <Form onSubmit={enviarMensaje}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Control placeholder="Ingrese su manesaje" name="texto" value={texto} onChange={handleTexto} />
-            </Form.Group>
-            <Button variant="info" type="submit" disabled={isLoading}>Enviar</Button>
-          </Form>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <h2>Jugando</h2>
-          <ListGroup variant="flush">
-            {
-              user.juegos && user.juegos.map(j =>
-                <ListGroup.Item key={j}>
-                  {j}
-                  <Button variant="warning" onClick={() => irJuego(j)} disabled={isLoading}>Ir al juego</Button>
-                </ListGroup.Item>
-              )
-            }
-          </ListGroup>
-        </Col>
-      </Row>
+      Hola {jugador.nombre}
+      <ListGroup variant="flush">
+        {
+          juego.jugadores.map(j => <ListGroup.Item key={j.nombre}>{j.nombre}</ListGroup.Item>)
+        }
+      </ListGroup>
+      <Button variant="danger" onClick={accionTerminarTurno}>Terminar turno</Button>
+      <Form onSubmit={atacar}>
+        <Form.Select name="numeroPaisO" onChange={handleAtaque}>
+          {
+            juego.paises.map(p => <option value={p.pais.numero} key={p.pais.numero}>{p.pais.nombre}</option>)
+          }
+        </Form.Select>
+
+        <Form.Select name="numeroPaisD" onChange={handleAtaque}>
+          {
+            juego.paises.map(p => <option value={p.pais.numero} key={p.pais.numero}>{p.pais.nombre}</option>)
+          }
+        </Form.Select>
+
+        <Button variant="info" type="submit" onClick={atacar}>Ataca</Button>
+      </Form>
+      <Alert>
+
+        Fase {
+          juego.turno.faseInicial && "Inicial"
+        }
+        {
+          juego.turno.fase4 && "4"
+        }
+        {
+          juego.turno.fase8 && "8"
+        }
+        {
+          juego.turno.faseJuego && "Juego"
+        }
+        {
+          juego.turno.faseReagrupar && "Reagrupar"
+        }
+        {
+          juego.turno.faseRefuerzos && "Refuerzos"
+        }
+      </Alert>
+      <Alert>
+        Turno {juego.jugadores[juego.turno.turno % juego.jugadores.length] &&
+          juego.jugadores[juego.turno.turno % juego.jugadores.length].nombre}
+      </Alert>
+      <Alert>
+        Fichas restantes {jugador.fichasRestantes}
+      </Alert>
+      <Stage width={1300} height={1182} draggable>
+        <Layer listening={false}>
+          <Image
+            image={mapa}
+            width={1600}
+            heigth={1182}
+            ref={node => node && node.cache()}
+            perfectDrawEnabled={false}
+          />
+        </Layer>
+        <Layer>
+          {
+            juego.paises.map(p =>
+              <Image key={p.pais.numero}
+                x={p.pais.posX}
+                y={p.pais.posY}
+                image={images[p.pais.archivo]}
+                //ref={node => drawHitFromCache(node, p)}
+                width={p.pais.width || 200}
+                height={p.pais.height || 200}
+                perfectDrawEnabled={false}
+                ref={imageRef}
+              />)
+          }
+        </Layer>
+        <Layer>
+          {
+            juego.paises.map(p =>
+              <Fragment key={p.pais.numero}>
+                <Text
+                  x={p.pais.posX}
+                  y={p.pais.posY}
+                  text={`fichas: ${p.fichas}`}
+                  onPointerClick={() => socketRef.current.emit('accionSimple', p.pais.numero, false)}
+                >
+                </Text>
+                <Text
+                  x={p.pais.posX}
+                  y={p.pais.posY + 20}
+                  text={`misiles: ${p.misiles}`}
+                  onPointerClick={() => socketRef.current.emit('accionSimple', p.pais.numero, false)}
+                >
+                </Text>
+              </Fragment>)
+          }
+        </Layer>
+      </Stage>
+      <JugadaInvalida
+        mensaje={jugadaInvalida}
+        show={show}
+        setShow={setShow}
+      />
+      <Button variant="warning" onClick={volverASala}>Volver a sala</Button>
+      <Button variant="danger" onClick={() => botonEmpate(params.nombreJuego)}>Boton empate</Button>
     </Container>
-  );
+  )
 }
 
-export default Sala;
+export default Mapa
